c16d8c2d335c47124cc4db553b14e4d59726df78
gojeta1 <guilhermebarbosa19@gmail.com>
2024-09-01 14:11:56 -0300

    updated disable button

--- index.js
+++ index.js
@@ -18,13 +18,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
         try {
             analyzeBtn.textContent = 'Analisando...';
-            analyzeBtn.disabled = true;
-            loadingIndicator.classList.remove('hidden');
             results.classList.add('hidden');
             await transcribeVideo(videoUrl);
         } catch (error) {
             alert('Ocorreu um erro ao analisar o vídeo: ' + error.message);
-            loadingIndicator.classList.add('hidden');
         } finally {
             analyzeBtn.textContent = 'Analisar Vídeo';
             analyzeBtn.disabled = false;
@@ -137,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function displayResults(recordData) {
-        loadingIndicator.classList.add('hidden');
+        analyzeBtn.disabled = true;
         results.classList.remove('hidden');
 
         suggestedTitle.innerHTML = formatText(recordData.fields.titulos) || 'Título não disponível';

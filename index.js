f26b3d548410b6a7d4cf9728e716a26c2fb4491b
gojeta1 <guilhermebarbosa19@gmail.com>
2024-09-01 15:37:43 -0300

    updated apiUrl

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     async function transcribeVideo(videoUrl) {
         try {
-            const apiUrl = 'https://copilot-yt.vercel.app/transcribe/transcribe';
+            const apiUrl = 'https://copilot-yt.vercel.app/transcribe';
 
             const response = await fetch(apiUrl, {
                 method: 'POST',
